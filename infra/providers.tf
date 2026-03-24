3d491c997b8b0d5d1fd79312fc29b2e35adf54a9
Akbar Belif, UPM <akbar.belif@visitor.upm.com>
2026-03-24 20:08:32 +0530

    revert the changes

--- infra/providers.tf
+++ infra/providers.tf
@@ -7,21 +7,24 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0, < 4.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 1.15"
+    }
     random = {
       source  = "hashicorp/random"
       version = "3.3.2"
     }
   }
-    backend "azurerm" {
-      resource_group_name  = "rg_azure_openai"
-      storage_account_name = "stgazureopenai101"
-      container_name       = "terraformstateprod"
-      key                  = "terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "azapi" {
+  # Reuse Azure CLI session established by azure/login in GitHub Actions.
+  use_cli = true
 }
 
 provider "random" {}
